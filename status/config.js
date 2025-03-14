a40efa6c5ba99a006c08f15366fbc55ca882d52b
Code3Astro <yuskophotographyoffical@gmail.com>
2025-03-14 03:30:29 -0400

    Update config.js

--- status/config.js
+++ status/config.js
@@ -1,17 +1,1 @@
-const config = {
-    servers: [
-        {
-            name: "Pines RP - Server 1",
-            joinLink: "ly8vlo" // Replace with your server's unique CFX identifier
-        },
-        {
-            name: "Pines RP - Server 2",
-            joinLink: "e7korm" // Replace with your second server's CFX identifier
-        }
-    ],
-    discordBot: {
-        name: "Pines RP Bot",
-        ip: "38.147.106.198",  // Replace with your bot's IP
-        port: "3054" // Replace with your bot's port
-    }
-};
+NOT IN USE
