9456bfdb56f552ae6583445fa9b2434201c87045
Code3Astro <yuskophotographyoffical@gmail.com>
2025-03-14 03:19:47 -0400

    Add files via upload

---
+++ status/config.js
@@ -0,0 +1,17 @@
+const config = {
+    servers: [
+        {
+            name: "Pines RP - Server 1",
+            ip: "158.180.47.93:25140" // Change this to your first FiveM server IP & port
+        },
+        {
+            name: "Pines RP - Server 2",
+            ip: "158.180.47.93:25140" // Change this to your second FiveM server IP & port
+        }
+    ],
+    discordBot: {
+        name: "Pines RP Bot",
+        ip: "38.147.106.198",  // Replace with your bot's IP
+        port: "3054" // Replace with your bot's port
+    }
+};
